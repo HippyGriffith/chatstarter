06af08aec1e2fa97b85aad9c8090684b38550f81
Sharon Griffith <sagriffith2021@gmail.com>
2024-11-04 02:30:49 -0700

    initialize Convex with messages API

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,45 @@
+"use client";
+import { api } from "@/convex/_generated/api";
+import { useQuery, useMutation } from "convex/react";
+import { useState } from "react";
+
+/*interface Message {
+  sender: string;
+  content: string;
+}*/
+
 export default function Home() {
-  return <div>Hello, world!</div>;
+  /*const [messages, setMessages] = useState<Message[]>([
+    { sender: "Alice", content: "Hello, world!" },
+    { sender: "Bob", content: "Hi, Alice!" },
+  ]);*/
+  const messages = useQuery(api.functions.message.list);
+  const createMessage = useMutation(api.functions.message.create);
+  const [input, setInput] = useState("");
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    //setMessages([...messages, { sender: "You", content: input }]);
+    void createMessage({ sender: "You", content: input });
+    setInput("");
+  };
+
+  return (
+    <div>
+      {messages?.map((message, index) => (
+        <div key={index}>
+          <strong>{message.sender}</strong>: {message.content}
+        </div>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="message"
+          id="message"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
 }
